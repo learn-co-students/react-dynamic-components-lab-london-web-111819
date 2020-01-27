f863bd0abb41c6054aab7548a2bd893e56511360
badogan <basri.dogan.71@gmail.com>
2020-01-27 10:15:40 +0000

    committed after fixing Comments

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,27 +3,16 @@ import React, { Component } from 'react';
 export default class ColorBox extends Component {
 
   render() {
-    return (
-      <div className="color-box" style={{opacity: 2}}>
-        {/* your conditional code here! */}
-        <ColorBox opacity={this.props.value}/>
+  
+    if (this.props.opacity >= 0.2) {
+      return (
+        <div className="color-box" style={{opacity: this.props.opacity}}>
+          {/* your conditional code here! */}
+          <ColorBox opacity={this.props.opacity - 0.1}/>
+        </div>
+      )
+    } else { return null}
 
-      </div>
-    )
   }
 
-}
-
-// ===============================
-<ColorBox opacity={1} />
-// =====
-import React, { Component } from 'react';
- 
-export default class Example extends Component {
-  render() {
-    const newValue = this.props.value * 2;
-    return this.props.value > 100 ? null : ( <div>
-        <Example value={newValue} />
-      </div>)
-  }
 }
