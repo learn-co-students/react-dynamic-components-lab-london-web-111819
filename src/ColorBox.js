480723612b00d0704174177786917aa2a8dde57d
badogan <basri.dogan.71@gmail.com>
2020-01-27 10:06:55 +0000

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -6,9 +6,24 @@ export default class ColorBox extends Component {
     return (
       <div className="color-box" style={{opacity: 2}}>
         {/* your conditional code here! */}
+        <ColorBox opacity={this.props.value}/>
+
       </div>
     )
   }
 
 }
 
+// ===============================
+<ColorBox opacity={1} />
+// =====
+import React, { Component } from 'react';
+ 
+export default class Example extends Component {
+  render() {
+    const newValue = this.props.value * 2;
+    return this.props.value > 100 ? null : ( <div>
+        <Example value={newValue} />
+      </div>)
+  }
+}
